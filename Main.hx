83ed6a6f60bdd625abc2c1027db2bbca8c4fd233
TheChickenIsBurnt <80657601+TheChickenIsBurnt@users.noreply.github.com>
2021-06-06 11:30:16 -1000

    Update Main.hx

--- Main.hx
+++ Main.hx
@@ -1,20 +1,42 @@
 class Main {
     static public function main():Void {
-        // variables
-        var firstNum:Int;
-        var secondNum:Int;
-        var boolVar:Bool;
-        // main
-        trace("first number:");
-        firstNum = Sys.stdin().readLine();
-        trace("second number:");
-        secondNum = Sys.stdin().readLine();
-        trace("Add/Subtract (true/false)");
-        boolVar = Sys.stdin().readLine();
-        if (boolop) {
-            trace(firstNum + secondNum);
-        } else {
-            trace(firstNum - secondNum);
-        }
+        var firstNum:String;
+
+       var secondNum:String;
+
+       var asmd:String;
+
+       trace("first number:");
+
+       firstNum = Sys.stdin().readLine();
+
+       trace("Second Number:");
+
+       secondNum = Sys.stdin().readLine();
+
+       var int1 = Std.parseInt(firstNum);
+
+       var int2 = Std.parseInt(secondNum);
+
+       trace("add, subtract, multiply, divide or mod:");
+
+       asmd = Sys.stdin().readLine();
+       
+       if (asmd == "add") {
+           trace(int1 + int2); 
+       }
+       else if (asmd == "subtract") {
+           trace(int1 - int2);
+       }
+       else if (asmd == "multiply") {
+           trace(int1 * int2); 
+       }
+       else if (asmd == "divide") {
+           trace(int1 / int2);
+       }
+       else if (asmd == "mod") {
+           trace(int1 % int2);
+       }
     }
+       
 }
